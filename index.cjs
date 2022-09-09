308fe6bd1b61c170e3e3324a807db9f8efd69a7a
Pelle Wessman <pelle@kodfabrik.se>
2022-09-09 21:16:55 +0200

    Print documentation url

--- index.cjs
+++ index.cjs
@@ -1,16 +1,18 @@
 /**
  * Generates formatted summary output from ESLint result set
  *
- * @param   {import('eslint').ESLint.LintResult[]} results  ESLint results
- * @returns {Promise<string>}                                        The formatted output
+ * @param   {import('eslint').ESLint.LintResult[]}   results  ESLint results
+ * @param   {import('eslint').ESLint.LintResultData} context
+ * @returns {Promise<string>}                                 The formatted output
  */
-module.exports = async function formatter(results) {
+module.exports = async function formatter(results, { rulesMeta }) {
   const { EFS_OUTPUT, EFS_SORT_BY, EFS_SORT_DESC } = process.env;
   const { format } = await import('./lib/format-results.js');
 
   return format(results, {
     output: EFS_OUTPUT,
     sortByProp: EFS_SORT_BY,
-    sortDescending: EFS_SORT_DESC === 'desc',
+    sortDescending: EFS_SORT_DESC === 'true',
+    rulesMeta
   });
 };
