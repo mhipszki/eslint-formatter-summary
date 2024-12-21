c47e67379d28b77231190454990ccf9ee8bee05e
Marton Hipszki <mhipszki@gmail.com>
2024-12-21 19:38:48 +0000

    chore: use module format for prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  plugins: ['prettier-plugin-packagejson'],
+};
+
+export default config;
