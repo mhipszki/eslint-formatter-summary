501f56cfdf0288a6abd0a825b83bcf7dbf9c86cd
Marton Hipszki <mhipszki@gmail.com>
2024-12-22 13:27:39 +0000

    refactor: co-locate processMessage with aggregator

--- lib/aggregator.spec.ts
+++ lib/aggregator.spec.ts
@@ -1,29 +1,83 @@
-import { test, expect } from 'vitest';
-import { aggregate } from './aggregator';
-import { mockLintResult } from './mock-result-factory';
+import { test, expect, describe } from 'vitest';
+import { aggregate, processMessage } from './aggregator';
+import { mockLintMessage, mockLintResult } from './mock-result-factory';
+import { Rule } from './length-of-longest';
 
-test('aggregates errors and warnings by rule', () => {
-  const results = [
-    mockLintResult([
-      ['rule1', 1],
-      ['rule2', 1],
-    ]),
-    mockLintResult([
-      ['rule2', 1],
-      ['rule3', 2],
-    ]),
-    mockLintResult([
-      ['rule3', 2],
-      ['rule4', 2],
-      ['rule5', 2],
-    ]),
-  ];
+describe('aggregate', () => {
+  test('aggregates errors and warnings by rule', () => {
+    const results = [
+      mockLintResult([
+        ['rule1', 1],
+        ['rule2', 1],
+      ]),
+      mockLintResult([
+        ['rule2', 1],
+        ['rule3', 2],
+      ]),
+      mockLintResult([
+        ['rule3', 2],
+        ['rule4', 2],
+        ['rule5', 2],
+      ]),
+    ];
 
-  expect(aggregate(results)).toEqual([
-    { ruleId: 'rule1', errors: 0, warnings: 1 },
-    { ruleId: 'rule2', errors: 0, warnings: 2 },
-    { ruleId: 'rule3', errors: 2, warnings: 0 },
-    { ruleId: 'rule4', errors: 1, warnings: 0 },
-    { ruleId: 'rule5', errors: 1, warnings: 0 },
-  ]);
+    expect(aggregate(results)).toEqual([
+      { ruleId: 'rule1', errors: 0, warnings: 1 },
+      { ruleId: 'rule2', errors: 0, warnings: 2 },
+      { ruleId: 'rule3', errors: 2, warnings: 0 },
+      { ruleId: 'rule4', errors: 1, warnings: 0 },
+      { ruleId: 'rule5', errors: 1, warnings: 0 },
+    ]);
+  });
+});
+
+describe('processMessage', () => {
+  test('adds new rules to summary', () => {
+    const summary: Rule[] = [];
+    expect(
+      processMessage(summary, mockLintMessage(['new rule', 1])),
+    ).toContainEqual({
+      ruleId: 'new rule',
+      errors: 0,
+      warnings: 1,
+    });
+    expect(
+      processMessage(summary, mockLintMessage(['another rule', 2])),
+    ).toContainEqual({
+      ruleId: 'another rule',
+      errors: 1,
+      warnings: 0,
+    });
+  });
+
+  // test('skips rules with no ruleId', () => {
+  //   const summary: Rule[] = [];
+  //   expect(processMessage(summary, mockLintMessage([undefined, 1]))).toEqual([]);
+  // });
+
+  test('updates errors / warnings of existing rules', () => {
+    const summary = processMessage([], mockLintMessage(['rule', 1]));
+    expect(
+      processMessage(summary, mockLintMessage(['rule', 1])),
+    ).toContainEqual({
+      ruleId: 'rule',
+      errors: 0,
+      warnings: 2,
+    });
+    expect(
+      processMessage(summary, mockLintMessage(['rule', 2])),
+    ).toContainEqual({
+      ruleId: 'rule',
+      errors: 1,
+      warnings: 2,
+    });
+  });
+
+  // test('maps ruleId=null to `syntax error`', () => {
+  //   expect(processMessage([], mockLintMessage([null, 2]))).toContainEqual({
+  //     ruleId: 'syntax error',
+  //     errors: 1,
+  //     warnings: 0,
+  //   });
+  // });
 });
--- lib/aggregator.ts
+++ lib/aggregator.ts
@@ -1,7 +1,28 @@
 import { ESLint } from 'eslint';
 import { extractMessages } from './extract-messages';
+import { Linter } from 'eslint';
+import { findRule } from './find-rule';
 import { Rule } from './length-of-longest';
-import { processMessage } from './process-message';
+
+export const processMessage = (
+  summary: Rule[],
+  message: Linter.LintMessage,
+) => {
+  if (typeof message.ruleId === 'undefined') return summary;
+
+  const ruleId = message.ruleId !== null ? message.ruleId : 'syntax error';
+  const severity = message.severity;
+  const errors = severity === 2 ? 1 : 0;
+  const warnings = severity === 1 ? 1 : 0;
+  const rule = findRule(summary, ruleId);
+  if (!rule) {
+    summary.push({ ruleId, errors, warnings });
+  } else {
+    rule.errors += errors;
+    rule.warnings += warnings;
+  }
+  return summary;
+};
 
 export const aggregate = (results: ESLint.LintResult[]): Rule[] =>
   extractMessages(results).reduce(processMessage, []);
